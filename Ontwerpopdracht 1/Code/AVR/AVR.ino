d8bd2eaaa34eca37416896390aaaf3fdd40f12c5
Sten Hulsbergen <stenhulsbergen@gmail.com>
2023-03-16 14:40:33 +0100

    pcb finaal + code

--- Ontwerpopdracht 1/Code/AVR/AVR.ino
+++ Ontwerpopdracht 1/Code/AVR/AVR.ino
@@ -1,7 +1,12 @@
-void setup() {
+#include <util/delay.h>
+#include <avr/io.h>
+#include "Arduino.h"
 
+void setup() {
+DDRB |= (1 << DDB5);
 }
 
-void loop() {
-
+void loop () {
+  PINB ^= (1 << PINB5);
+  _delay_ms(500);
 }
