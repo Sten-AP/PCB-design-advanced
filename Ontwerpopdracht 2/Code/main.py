4c006d88a12f6cd488f209ee3b8fbfc624a2308f
Sten H <94611203+Sten-AP@users.noreply.github.com>
2023-05-18 19:02:06 +0200

    Update main.py

--- Ontwerpopdracht 2/Code/main.py
+++ Ontwerpopdracht 2/Code/main.py
@@ -4,7 +4,6 @@ import board
 import time
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
