9c6ab62e6dea754727a2dadd0e37df0294a7a161
Sten Hulsbergen <94611203+Sten-AP@users.noreply.github.com>
2023-05-31 15:48:19 +0200

    Update main.ino

--- Ontwerpopdracht 1/Code/Programmatie/main/main.ino
+++ Ontwerpopdracht 1/Code/Programmatie/main/main.ino
@@ -1,50 +1,51 @@
-// #include <tinyNeoPixel.h>
+#include <tinyNeoPixel.h>
 
-// #define LED_PIN 3
-// #define NUM_LEDS 12
+#define LED_PIN 3
+#define NUM_LEDS 12
 
-// tinyNeoPixel pixels(NUM_LEDS, PIN_PA5, NEO_GRB);
-// int mosfet = 9;
-// // #define mosfet 9 //PB0 MOSFET_PIN
-
-// void setup() {
-//   pinMode(mosfet, OUTPUT);
-//   digitalWrite(mosfet, HIGH); 
-
-//   pixels.begin();
-//   pixels.setPixelColor(0, pixels.Color(0, 10, 0));
-//   // pixels.setPixelColor(1, pixels.Color(0, 255, 0));
-
-//   pixels.show();
-// }
-
-// void loop() {
-//   // Code hier
-// }
-
-
-
-
-#include <Adafruit_NeoPixel.h>
-
-#define PIN_UREN 10 //PA5 LEDS uren
-#define PIN_MINUTEN 13 //PA3 LEDS minuten
-#define AANTAL_LEDS 12
-
-int mosfet = 9; //PB0 MOSFET_PIN
-
-Adafruit_NeoPixel LEDS_UREN(AANTAL_LEDS, PIN_UREN, NEO_GRB + NEO_KHZ800);
-// Adafruit_NeoPixel LEDS_MINUTEN(AANTAL_LEDS, PIN_MINUTEN, NEO_GRB + NEO_KHZ800);
+tinyNeoPixel pixels(NUM_LEDS, PIN_PA5, NEO_GRB);
+int mosfet = 9;
+// #define mosfet 9 //PB0 MOSFET_PIN
 
 void setup() {
   pinMode(mosfet, OUTPUT);
+  digitalWrite(mosfet, LOW); 
 
-  LEDS_UREN.begin();
-  LEDS_UREN.setPixelColor(1, 150, 0, 0);
-  LEDS_UREN.show();
+  pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(0, 10, 0));
+  // pixels.setPixelColor(1, pixels.Color(0, 255, 0));
 
-  digitalWrite(mosfet, HIGH); 
+  pixels.show();
 }
 
 void loop() {
+  // Code hier
 }
+
+
+
+
+// #include <Adafruit_NeoPixel.h>
+
+// #define PIN_UREN 10 //PA5 LEDS uren
+// #define PIN_MINUTEN 13 //PA3 LEDS minuten
+// #define AANTAL_LEDS 12
+
+// int mosfet = 9; //PB0 MOSFET_PIN
+
+// Adafruit_NeoPixel LEDS_UREN(AANTAL_LEDS, PIN_UREN, NEO_GRB + NEO_KHZ800);
+// // Adafruit_NeoPixel LEDS_MINUTEN(AANTAL_LEDS, PIN_MINUTEN, NEO_GRB + NEO_KHZ800);
+
+// void setup() {
+//   pinMode(mosfet, OUTPUT);
+
+//   LEDS_UREN.begin();
+//   LEDS_UREN.setPixelColor(1, 150, 0, 0);
+  
+//   LEDS_UREN.show();
+
+//   digitalWrite(mosfet, HIGH); 
+// }
+
+// void loop() {
+// }
